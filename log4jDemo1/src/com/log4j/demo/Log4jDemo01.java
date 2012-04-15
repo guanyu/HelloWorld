b066755d219cd7c60086bfb6a4c437e875633b4c
guanyu_yan <603741418@qq.com>
2012-04-15 13:43:56 +0800

    删掉注释

--- log4jDemo1/src/com/log4j/demo/Log4jDemo01.java
+++ log4jDemo1/src/com/log4j/demo/Log4jDemo01.java
@@ -2,11 +2,6 @@ package com.log4j.demo;
 import org.apache.log4j.Logger;
 
 
-
-/**
- * @author admin
- *
- */
 public class Log4jDemo01 {
 	
 	private static Logger logger = Logger.getLogger(Log4jDemo01.class); 
